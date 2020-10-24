7cf9111bfc1728c7aec5ce278d3b4d888c609306
camisaenz510 <68917860+camisaenz510@users.noreply.github.com>
2020-10-24 16:29:43 -0600

    main

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,51 @@
 // Created by Owner on 08/10/2020.
 //
 
-#include <../lib/nlohmann/json.hpp>
+
 #include <iostream>
 #include <sstream>
-
-
-using namespace std;
-using json = nlohmann::json;
+#include "PersonAdministrator.h"
+#include "InBinary.h"
+#include "InJSON.h"
 
 int main() {
 
-    //JSON
+    //creating the person
+    Person person_1("Fabiola", 1010, 19);
+    Person person_2("Maria", 1020, 30);
+    Person person_3("Adrian", 1030, 10);
+
+    //creating the list
+    vector<Person> personList;
+    personList.push_back(person_1);
+    personList.push_back(person_2);
+    personList.push_back(person_3);
+
+    //creating the archive
+    PersonAdministrator *admin;
+    InBinary* binary=new InBinary();
+    InJSON* json=new InJSON();
+
+    //administrator
+    admin->savingPersons(binary, personList);
+    admin->savingPersons(json, personList);
+
+    //in binary
+    cout<<" SHOWING IN BINARY"<<std::endl;
+    vector<Person> aux= admin->reading(binary);
+    for (Person auxPerson: aux){
+        cout<<auxPerson.toString()<<std::endl;
+    }
+
+    //in json
+    cout<<" SHOWING IN JSON"<<std::endl;
+    vector<Person> auxi;
+    auxi=admin->reading(json);
+    for(Person auxPerson: auxi){
+        cout<<auxPerson.toString()<<std::endl;
+    }
+
+
 
     return 0;
 }
